3a4bb6c1042363c5073803a9642c0e06c7cf31b1
Louis-Philippe Gauthier <lpgauth@gmail.com>
2013-09-08 10:18:05 -0400

    Update dashboard style

--- src/toppage_handler.erl
+++ src/toppage_handler.erl
@@ -13,12 +13,11 @@ init(_Transport, Req, []) ->
     {ok, Req, #state {}}.
 
 handle(Req, State) ->
-    Headers = [
-        {<<"Content-Type">>, <<"text/html">>}
-    ],
-    {ok, Template} = video_dashboard_dtl:render(),
-    {ok, Req2} = cowboy_req:reply(200, Headers, Template, Req),
-    {ok, Req2, State}.
+    Headers = [{<<"Content-Type">>, <<"text/html">>}],
+    {Querystring, Req2} = cowboy_req:qs(Req),
+    {ok, Template} = video_dashboard_dtl:render([{querystring, Querystring}]),
+    {ok, Req3} = cowboy_req:reply(200, Headers, Template, Req2),
+    {ok, Req3, State}.
 
 terminate(_Reason, _Req, _State) ->
     ok.
