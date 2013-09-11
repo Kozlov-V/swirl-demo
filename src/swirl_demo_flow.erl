25312d4cc76adca6dd43df3395f6b947657bcd54
Louis-Philippe Gauthier <lpgauth@gmail.com>
2013-09-10 22:48:27 -0400

    Rename counters to aggregates

--- src/swirl_demo_flow.erl
+++ src/swirl_demo_flow.erl
@@ -11,6 +11,6 @@ map(_StreamName, Event, _MapperOpts) ->
     Type = swirl_utils:lookup(type, Event),
     {update, {Type}, {1}}.
 
-reduce(Period, Counters, ReducerOpts) ->
+reduce(Period, Aggregates, ReducerOpts) ->
     Pid = swirl_utils:lookup(send_to, ReducerOpts),
-    Pid ! {flow, Period, Counters}.
+    Pid ! {flow, Period, Aggregates}.
